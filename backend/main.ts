17d494b564d3087a553e0fe772acdb497c9ab490
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-05 17:19:05 +0530

    chore: Setup Prisma ORM and local SQLite database

--- backend/main.ts
+++ backend/main.ts
@@ -1,6 +1,8 @@
 import { poweredBy } from "hono/powered-by"
 import { Hono } from "hono"
 
+import { prisma } from "./src/db/client"
+
 const app = new Hono()
 
 app.use(poweredBy())
@@ -8,3 +10,6 @@ app.use(poweredBy())
 app.get("/api/v1", (c) => c.json({ message: "Hello, from my REST API!" }))
 
 export default app
+
+const users = await prisma.user.findMany()
+console.log(users)
