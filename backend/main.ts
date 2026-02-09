450ac552d71b37fbdb3f163bdc0b24004abd0b61
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-09 12:51:39 +0530

    feat: Global error handler

--- backend/main.ts
+++ backend/main.ts
@@ -1,3 +1,4 @@
+import { HTTPException } from "hono/http-exception"
 import { poweredBy } from "hono/powered-by"
 import { Hono } from "hono"
 
@@ -6,6 +7,15 @@ import { authRoutes } from "@/routes/auth.routes"
 const app = new Hono()
 
 app.use(poweredBy())
+
+app.onError((err, c) => {
+  if (err instanceof HTTPException) {
+    console.log(err)
+    return err.getResponse()
+  }
+  console.log(err)
+  return c.json({ message: "Someting went wrong." }, 500)
+})
 
 app.get("/api/v1", (c) => c.json({ message: "Hello, from my REST API!" }))
 
