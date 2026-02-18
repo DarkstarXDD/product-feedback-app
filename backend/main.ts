dd1510435788981e0975c8b7a65d28d975da15c5
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-18 12:49:16 +0530

    refactor: Update protected and public route logic (#7)

--- backend/main.ts
+++ backend/main.ts
@@ -6,20 +6,52 @@ import { Hono } from "hono"
 import authRoutes from "@/routes/auth.routes"
 import userRoutes from "@/routes/user.routes"
 
+// const app = new Hono()
+// const publicRoutes = new Hono()
+// const protectedRoutes = new Hono()
+
+// /** poweredBy middleware runs on all routes. */
+// app.use(poweredBy())
+
+// /** Global error handler. */
+// app.onError((err, c) => {
+//   if (err instanceof HTTPException) {
+//     console.log(err)
+//     return err.getResponse()
+//   }
+//   console.log(err)
+//   return c.json({ message: "Someting went wrong." }, 500)
+// })
+
+// /** JWT middleware runs on all routes that are mounted in the protectedRoutes Hono instance.  */
+// protectedRoutes.use(jwt({ secret: JWT_SECRET, cookie: "token", alg: "HS256" }))
+
+// /** Mount `authRoutes` Hono instance at `/auth` base path, on the publicRoutes Hono instance.  */
+// publicRoutes.route("/auth", authRoutes)
+
+// /** A GET handler mounted on the `/` base path on the publicRoutes Hono instance.  */
+// publicRoutes.get("/", (c) => c.json({ message: "Success" }))
+
+// /** Mount `userRoutes` Hono instance on the `/users` base path on the protectedRoutes Hono instance.
+//  * Because we are running the JWT middleware on all routes on the protectedRoutes instance, any route instance mounted on protectedRoutes will be protected by default.
+//  */
+// protectedRoutes.route("/users", userRoutes)
+
+// app.route("/api/v1", publicRoutes)
+// app.route("/api/v1", protectedRoutes)
+
 const JWT_SECRET = process.env.JWT_SECRET
 if (!JWT_SECRET) throw new Error("JWT_SECRET is not defined.")
 
 const app = new Hono()
-const publicRoutes = new Hono()
-const protectedRoutes = new Hono()
+const api = new Hono()
 
+const authMw = jwt({ secret: JWT_SECRET, cookie: "token", alg: "HS256" })
+
+/** poweredBy middleware runs on all routes. */
 app.use(poweredBy())
 
-protectedRoutes.use(
-  "*",
-  jwt({ secret: JWT_SECRET, cookie: "token", alg: "HS256" })
-)
-
+/** Global error handler. */
 app.onError((err, c) => {
   if (err instanceof HTTPException) {
     console.log(err)
@@ -29,12 +61,19 @@ app.onError((err, c) => {
   return c.json({ message: "Someting went wrong." }, 500)
 })
 
-publicRoutes.get("/", (c) => c.json({ message: "Success" }))
-publicRoutes.route("/auth", authRoutes)
+/** All routes on `api` are public by default. Mount `auth` instance on it. */
+api.route("/auth", authRoutes)
+api.get("/", (c) => c.json({ message: "Success" }))
 
-protectedRoutes.route("/users", userRoutes)
+/** Apply auth middleware to only needed routes. So these are protected routes. */
+api.use("/users/*", authMw)
+api.use("/posts/*", authMw)
+api.use("/comments/*", authMw)
 
-app.route("/api/v1", publicRoutes)
-app.route("/api/v1", protectedRoutes)
+/** Mount instanes on protected routes. */
+api.route("/users", userRoutes)
+
+/** Mount API instance on app. */
+app.route("/api/v1", api)
 
 export default app
