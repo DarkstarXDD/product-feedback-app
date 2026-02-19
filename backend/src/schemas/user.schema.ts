60a156c04e9522e846143c3e2321ba279147cefa
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-19 19:18:21 +0530

    refactor: Create a base userSchema

---
+++ backend/src/schemas/user.schema.ts
@@ -0,0 +1,14 @@
+import * as z from "zod"
+
+/** Base schema for user. */
+export const userSchema = z.object({
+  username: z
+    .string("Invalid username")
+    .min(1, "Username cannot be empty")
+    .max(30, "Username cannot have more than 30 characters"),
+  name: z
+    .string("Invalid name")
+    .min(1, "Name cannot be empty")
+    .max(50, "Name cannot have more than 50 characters"),
+  email: z.email("Invalid email").toLowerCase(),
+})
