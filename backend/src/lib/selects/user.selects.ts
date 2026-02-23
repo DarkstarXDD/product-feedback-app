86dcb96771447f612051a17dbd7b6b4148e01c63
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-23 17:22:55 +0530

    refactor: Update privateUserSelect fields

--- backend/src/lib/selects/user.selects.ts
+++ backend/src/lib/selects/user.selects.ts
@@ -6,9 +6,14 @@ export const publicUserSelect = {
 } as const satisfies Prisma.UserSelect
 
 export const privateUserSelect = {
+  createdAt: true,
+  updatedAt: true,
   username: true,
+  _count: true,
   email: true,
+  role: true,
   name: true,
+  id: true,
 } as const satisfies Prisma.UserSelect
 
 export type PublicUser = Prisma.UserGetPayload<{
