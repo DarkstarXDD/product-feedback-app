705bcc0129455a1431d2b0e5d29d18b9504a99c3
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-25 10:34:03 +0530

    feat: Setup Vitest for testing

---
+++ backend/vitest.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from "vitest/config"
+import { fileURLToPath } from "node:url"
+import { loadEnv } from "vite"
+import path from "node:path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default defineConfig(({ mode }) => {
+  return {
+    // https://vitest.dev/guide/features#environment-variables
+    test: {
+      env: loadEnv(mode, __dirname, ""),
+      // isolate: false,
+      // pool: "forks",
+      // maxWorkers: 1,
+    },
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+    },
+  }
+})
