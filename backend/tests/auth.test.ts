d66f9f7885bdd721cddaaa235566469392594ce5
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-02-25 21:06:20 +0530

    test: 201 success

--- backend/tests/auth.test.ts
+++ backend/tests/auth.test.ts
@@ -4,6 +4,16 @@ import authRoutes from "@/routes/auth.routes"
 
 import { createDummyUserData } from "./utils"
 
+type SignupResponse = {
+  data: {
+    createdAt: string
+    username: string
+    email: string
+    name: string
+    id: string
+  }
+}
+
 describe("POST /api/v1/auth/signup", () => {
   test("returns 201 and created user for valid payload", async () => {
     const payload = createDummyUserData()
@@ -13,6 +23,22 @@ describe("POST /api/v1/auth/signup", () => {
       body: JSON.stringify(payload),
       method: "POST",
     })
+
+    const resBody = (await res.json()) as SignupResponse
+
+    /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+    expect(resBody).toEqual({
+      data: expect.objectContaining({
+        email: payload.email.toLowerCase(),
+        createdAt: expect.any(String),
+        username: payload.username,
+        id: expect.any(String),
+        name: payload.name,
+      }),
+    })
+
+    expect(resBody.data).not.toHaveProperty("password")
+    expect(resBody.data).not.toHaveProperty("confirmPassword")
 
     expect(res.status).toBe(201)
   })
