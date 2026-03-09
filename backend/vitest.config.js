dea2f1246c25ebd7224c46cae471e58404d73aa7
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-03-09 22:22:13 +0530

    chore: Make vitest reports verbose

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -17,6 +17,7 @@ export default defineConfig(({ mode }) => {
     // https://vitest.dev/guide/features#environment-variables
     test: {
       env: loadEnv(mode, __dirname, ""),
+      reporters: ["verbose"],
     },
   }
 })
