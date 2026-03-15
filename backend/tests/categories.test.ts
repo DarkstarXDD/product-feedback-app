547b73c7b212b33cf59e7b6773317dc7ed00eb1c
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-03-15 17:25:55 +0530

    test: Test for get categories route

---
+++ backend/tests/categories.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test } from "vitest"
+
+import type { JsonSuccessBody } from "@/lib/utils"
+
+import { type Category } from "@/lib/selects/category.selects"
+
+import app from "../main"
+
+describe("GET /api/v1/categories", () => {
+  test.only("returns 200 and category list", async () => {
+    const res = await app.request("/api/v1/categories")
+
+    const resBody = (await res.json()) as JsonSuccessBody<Category[]>
+
+    expect(res.status).toBe(200)
+    expect(Array.isArray(resBody.data)).toBe(true)
+    expect(resBody.data.length).toBeGreaterThan(0)
+    expect(resBody.data[0]).toHaveProperty("id")
+    expect(resBody.data[0]).toHaveProperty("name")
+    expect(resBody.data[0]).toHaveProperty("slug")
+  })
+})
