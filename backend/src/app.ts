df8997b43e498ab89a356857b0d671bee0cc844e
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-03-18 12:57:02 +0530

    feat: Return json for all not found routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,12 +9,22 @@ import commentsRouter from "@/routes/comments.route"
 import statusRoutes from "@/routes/status.routes"
 import authRoutes from "@/routes/auth.routes"
 import userRoutes from "@/routes/user.routes"
+import { jsonError } from "@/lib/utils"
 
 const app = new Hono()
 const api = new Hono()
 
 app.use(poweredBy())
 app.use(logger())
+
+/** By default Hono returns a text response for notFound. I wanted a JSON response. */
+app.notFound((c) => {
+  return jsonError(
+    c,
+    { message: "Not found", code: "NOT_FOUND" },
+    { status: 404 }
+  )
+})
 
 /** Global error handler. */
 app.onError((err, c) => {
