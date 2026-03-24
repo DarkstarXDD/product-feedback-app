533f25c44342575a1d9ab55ed4b79a9117ed4619
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-03-24 14:43:03 +0530

    refactor: Update comment schema

--- backend/src/schemas/comments.schema.ts
+++ backend/src/schemas/comments.schema.ts
@@ -5,5 +5,9 @@ export const commentCreateSchema = z.object({
   content: z
     .string("Invalid comment")
     .min(1, "Comment cannot be empty")
-    .max(500, "Comment cannot have more than 500 characters"),
+    .max(500, "Comment cannot have more than 500 characters")
+    .meta({
+      example: "I would love to see this added.",
+      description: "Comment content.",
+    }),
 })
