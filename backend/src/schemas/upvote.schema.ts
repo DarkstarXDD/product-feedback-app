7e8dcfc28d96d755831ac33091c28ae350e14743
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-03-29 14:56:36 +0530

    feat: Add upvoteResponse schema

---
+++ backend/src/schemas/upvote.schema.ts
@@ -0,0 +1,25 @@
+import * as z from "zod"
+
+import type { UpvoteResponse } from "@/lib/selects/upvote.select"
+
+// --------------------- Upvote Response Schema -----------------------
+export const upvoteResponseSchema: z.ZodType<UpvoteResponse> = z.object({
+  id: z.cuid().meta({
+    pattern: undefined,
+    example: "cmlubyi3l000094r6fw9v8djs",
+    "x-order": 1,
+  }),
+  createdAt: z
+    .date()
+    .meta({ example: "2026-01-01T00:00:00.000Z", "x-order": 2 }),
+  userId: z.cuid().meta({
+    pattern: undefined,
+    example: "cmlubyi3l000094r6fw9v8djs",
+    "x-order": 3,
+  }),
+  suggestionId: z.cuid().meta({
+    pattern: undefined,
+    example: "cmlubyi3l000094r6fw9v8djs",
+    "x-order": 4,
+  }),
+})
