8b1e930fa3a77fbfb6218e6d1fcc6b1eba3660f6
DarkstarXDD <108657985+DarkstarXDD@users.noreply.github.com>
2026-04-18 22:23:22 +0530

    refactor: Simplify env validation setup (#77)

--- backend/src/lib/env.ts
+++ backend/src/lib/env.ts
@@ -3,26 +3,19 @@ import * as z from "zod"
 import { formatZodErrors } from "@/lib/utils"
 
 const envSchema = z.object({
-  JWT_SECRET: z.string("JWT Secret is not defined"),
-  DATABASE_URL: z.url("Database URL is not defined"),
   NODE_ENV: z.string().default("development"),
+  JWT_SECRET: z.string("JWT Secret is not defined"),
+  DATABASE_URL: z.url("Database URL is invalid or not defined"),
+  DIRECT_URL: z.url("Direct URL is invalid or not defined"),
 })
 
-type Env = z.infer<typeof envSchema>
+const parsed = envSchema.safeParse(process.env)
 
-let env: Env
-
-try {
-  env = envSchema.parse(process.env)
-} catch (e) {
-  if (e instanceof z.ZodError) {
-    const errors = formatZodErrors(e)
-    console.log("Error loading environment variables!")
-    console.log(errors.fieldErrors)
-  } else {
-    throw new Error("Error loading environment variables!")
-  }
+if (!parsed.success) {
+  console.log("Error loading environment variables!")
+  console.log(formatZodErrors(parsed.error).fieldErrors)
   process.exit(1)
 }
 
+const env = parsed.data
 export default env
